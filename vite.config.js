ba901b057027c9e5c92f30e12b4c47a936027c58
elcanmuradov <elcanmuradov25@gmail.com>
2026-05-08 21:48:36 +0400

    2

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://api.netbazar.tech',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
